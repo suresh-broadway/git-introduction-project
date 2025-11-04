9363f8c91d675264a642162aad0654bb47319e37
Suresh Upreti <sureshupreti877@gmail.com>
2025-11-04 07:57:12 +0545

    add the sum file

---
+++ src/main/java/test/sum.java
@@ -0,0 +1,11 @@
+package test;
+
+public class sum {
+    static int a =5;
+    static int b=6;
+    static int sum1 = a+b;
+
+    public static void main(String[] args) {
+        System.out.println("The sum of two number is:"+sum1);
+    }
+}
